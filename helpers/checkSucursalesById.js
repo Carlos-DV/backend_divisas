7ffedbebb90be0b0dc68913a15983ee439f90b90
carlos-dv <gear_240@hotmail.com>
2023-04-29 14:41:51 -0600

    Refactor code if in sucursales

--- helpers/checkSucursalesById.js
+++ helpers/checkSucursalesById.js
@@ -1,12 +1,39 @@
 import { sql, getConnectionChampoton } from "../database/configDB.js"
 import checkQueryTipoCambio from "./checkQueryTipoCambio.js"
 
+
+const connectionMap = {
+    // 1: getConnectionTalleres,
+    // 2: getConnectionConstituyentes,
+    // 3: getConnectionNichupte,
+    // 4: getConnectionRanchoViejo,
+    // 5: getConnectionTulum,
+    // 6: getConnectionCozumel,
+    // 7: getConnectionPortillo,
+    // 9: getConnectionAleman,
+    // 10: getConnectionFederal,
+    // 11: getConnectionValladolid,
+    // 12: getConnectionPeriferico,
+    // 13: getConnectionPtoMorelos,
+    // 14: getConnectionGobernadores,
+    // 15: getConnectionLakin,
+    // 16: getConnectionTizimin,
+    // 17: getConnectionHuayakan,
+    // 18: getConnectionProgreso,
+    // 19: getConnectionCanek,
+    // 20: getConnection50Sur,
+    // 23: getConnectionTrueba,
+    24: getConnectionChampoton,
+  };
+
+  
 const checkSucursalesById = async function(sucursalId, idSucursal, res) {
-    if(sucursalId === 24){
-        const pool = await getConnectionChampoton()
+    const getConnection = connectionMap[sucursalId];
+    if(getConnection){
+        const pool = await getConnection()
         checkQueryTipoCambio(idSucursal, res, pool)     
     }else {
-        console.log('no se encuentra')
+        console.log('no se encuentra el id de la sucursal')
     }
 }
 
