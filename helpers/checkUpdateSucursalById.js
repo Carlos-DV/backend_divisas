82440ab5874b026f6e074ef39f927007635c6ffd
carlos-dv <gear_240@hotmail.com>
2023-04-30 16:45:14 -0600

    update with new refactor code

--- helpers/checkUpdateSucursalById.js
+++ helpers/checkUpdateSucursalById.js
@@ -1,16 +1,42 @@
-import { getConnectionChampoton, sql, } from "../database/configDB.js"
+import { sql, getConnectionChampoton, getConnectionTrueba, getConnectionTalleres, getConnectionConstituyentes, getConnectionNichupte, getConnectionRanchoViejo, getConnectionTulum, getConnectionCozumel, getConnectionPortillo, getConnectionAleman, getConnectionFederal, getConnectionValladolid, getConnectionPeriferico, getConnectionPtoMorelos, getConnectionGobernadores, getConnectionLakin, getConnectionTizimin, getConnectionHuayakan, getConnectionProgreso, getConnectionCanek, getConnection50Sur } from "../database/configDB.js"
 import checkQueryUpdateTipoCambio from "./checkQueryUpdateTipoCambio.js"
+
+
+const connectionMap = {
+    1: getConnectionTalleres,
+    2: getConnectionConstituyentes,
+    3: getConnectionNichupte,
+    4: getConnectionRanchoViejo,
+    5: getConnectionTulum,
+    6: getConnectionCozumel,
+    7: getConnectionPortillo,
+    9: getConnectionAleman,
+    10: getConnectionFederal,
+    11: getConnectionValladolid,
+    12: getConnectionPeriferico,
+    13: getConnectionPtoMorelos,
+    14: getConnectionGobernadores,
+    15: getConnectionLakin,
+    16: getConnectionTizimin,
+    17: getConnectionHuayakan,
+    18: getConnectionProgreso,
+    19: getConnectionCanek,
+    20: getConnection50Sur,
+    23: getConnectionTrueba,
+    24: getConnectionChampoton,
+  };
 
 
 const checkUpdateSucursalById = async function(sucursalId, idSucursal, req, res) {
     // console.log(sucursalId)
-    // return
-    if(sucursalId === 24){
-        const pool = await getConnectionChampoton()
-        checkQueryUpdateTipoCambio(idSucursal, req, res, pool)
+    const getConnection = connectionMap[sucursalId];
+    if(getConnection){
+        const pool = await getConnection()
+        checkQueryUpdateTipoCambio(idSucursal, req, res, pool) 
     }else {
-        console.log('no se encuentra PUT')
+        console.log('no se encuentra el id de la sucursal')
     }
+    
 }
 
 export default checkUpdateSucursalById
