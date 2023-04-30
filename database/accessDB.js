9f2cc703556ce719e0368ff4f796f4670dc98df0
carlos-dv <gear_240@hotmail.com>
2023-04-30 16:26:48 -0600

    finished the connection with the branches

--- database/accessDB.js
+++ database/accessDB.js
@@ -16,8 +16,47 @@ const createDbSettings = (ipBranch) => {
 
 const dbSettingsChampoton = createDbSettings(process.env.IP_CHAMPOTON);
 const dbSettingsTrueba = createDbSettings(process.env.IP_TRUEBA);
+const dbSettings50Sur = createDbSettings(process.env.IP_SUR);
+const dbSettingsCanek = createDbSettings(process.env.IP_CANEK);
+const dbSettingsProgreso = createDbSettings(process.env.IP_PROGRESO);
+const dbSettingsHuayakan  = createDbSettings(process.env.IP_HUAYAKAN);
+const dbSettingsTizimin = createDbSettings(process.env.IP_TIZIMIN);
+const dbSettingsLakin = createDbSettings(process.env.IP_LAKIN);
+const dbSettingsGobernadores = createDbSettings(process.env.IP_GOBERNADORES);
+const dbSettingsPtoMorelos  = createDbSettings(process.env.IP_PTOMORELOS);
+const dbSettingsPeriferico = createDbSettings(process.env.IP_PERIFERICO);
+const dbSettingsValladolid = createDbSettings(process.env.IP_VALLADOLID);
+const dbSettingsFederal = createDbSettings(process.env.IP_FEDERAL);
+const dbSettingsAleman = createDbSettings(process.env.IP_ALEMAN);
+const dbSettingsPortillo = createDbSettings(process.env.IP_PORTILLO);
+const dbSettingsCozumel = createDbSettings(process.env.IP_COZUMEL);
+const dbSettingsTulum = createDbSettings(process.env.IP_TULUM);
+const dbSettingsRanchoViejo = createDbSettings(process.env.IP_RANCHOVIEJO);
+const dbSettingsNichupte = createDbSettings(process.env.IP_NICHUPTE);
+const dbSettingsConstituyentes = createDbSettings(process.env.IP_CONSTITUYENTES);
+const dbSettingsTalleres = createDbSettings(process.env.IP_TALLERES);
+
 
   export {
     dbSettingsChampoton,
     dbSettingsTrueba,
+    dbSettings50Sur,
+    dbSettingsCanek,
+    dbSettingsProgreso,
+    dbSettingsHuayakan,
+    dbSettingsTizimin,
+    dbSettingsLakin,
+    dbSettingsGobernadores,
+    dbSettingsPtoMorelos,
+    dbSettingsPeriferico,
+    dbSettingsValladolid,
+    dbSettingsFederal,
+    dbSettingsAleman,
+    dbSettingsPortillo,
+    dbSettingsCozumel,
+    dbSettingsTulum,
+    dbSettingsRanchoViejo,
+    dbSettingsNichupte,
+    dbSettingsConstituyentes,
+    dbSettingsTalleres,
 }
--- database/configDB.js
+++ database/configDB.js
@@ -1,5 +1,5 @@
 import sql from 'mssql'
-import { dbSettingsChampoton, dbSettingsTrueba } from './accessDB.js';
+import { dbSettings50Sur, dbSettingsAleman, dbSettingsCanek, dbSettingsChampoton, dbSettingsConstituyentes, dbSettingsCozumel, dbSettingsFederal, dbSettingsGobernadores, dbSettingsHuayakan, dbSettingsLakin, dbSettingsNichupte, dbSettingsPeriferico, dbSettingsPortillo, dbSettingsProgreso, dbSettingsPtoMorelos, dbSettingsRanchoViejo, dbSettingsTalleres, dbSettingsTizimin, dbSettingsTrueba, dbSettingsTulum, dbSettingsValladolid } from './accessDB.js';
 
 export async function getConnection(dbSettings) {
   const pool = new sql.ConnectionPool(dbSettings);
@@ -7,7 +7,7 @@ export async function getConnection(dbSettings) {
     await pool.connect();
     return pool;
   } catch (error) {
-    console.log(`Error: ${error} --config`);
+    // console.log(`Error: ${error} --config`);
     return { error: 'No se pudo establecer conexión con la base de datos' };
   }
 }
@@ -15,9 +15,65 @@ export async function getConnection(dbSettings) {
 export async function getConnectionChampoton() {
   return getConnection(dbSettingsChampoton);
 }
-
 export async function getConnectionTrueba() {
   return getConnection(dbSettingsTrueba);
+}
+export async function getConnection50Sur() {
+  return getConnection(dbSettings50Sur)
+}
+export async function getConnectionCanek() {
+  return getConnection(dbSettingsCanek)
+}
+export async function getConnectionProgreso() {
+  return getConnection(dbSettingsProgreso)
+}
+export async function getConnectionHuayakan() {
+  return getConnection(dbSettingsHuayakan)
+}
+export async function getConnectionTizimin() {
+  return getConnection(dbSettingsTizimin)
+}
+export async function getConnectionLakin() {
+  return getConnection(dbSettingsLakin)
+}
+export async function getConnectionGobernadores() {
+  return getConnection(dbSettingsGobernadores)
+}
+export async function getConnectionPtoMorelos() {
+  return getConnection(dbSettingsPtoMorelos)
+}
+export async function getConnectionPeriferico() {
+  return getConnection(dbSettingsPeriferico)
+}
+export async function getConnectionValladolid() {
+  return getConnection(dbSettingsValladolid)
+}
+export async function getConnectionFederal() {
+  return getConnection(dbSettingsFederal)
+}
+export async function getConnectionAleman() {
+  return getConnection(dbSettingsAleman)
+}
+export async function getConnectionPortillo() {
+  return getConnection(dbSettingsPortillo)
+}
+export async function getConnectionCozumel() {
+  return getConnection(dbSettingsCozumel)
+}
+export async function getConnectionTulum() {
+  return getConnection(dbSettingsTulum)
+}
+export async function getConnectionRanchoViejo() {
+  return getConnection(dbSettingsRanchoViejo)
+}
+export async function getConnectionNichupte() {
+  return getConnection(dbSettingsNichupte)
+}
+export async function getConnectionConstituyentes() {
+  return getConnection(dbSettingsConstituyentes)
+}
+export async function getConnectionTalleres() {
+  return getConnection(dbSettingsTalleres)
 }
 
 
